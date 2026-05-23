bb4f83daac02421e5c9728caadc9427b09e81d5a
Minh Nguyễn <nguyenducminh.dev@gmail.com>
2026-05-23 10:25:15 +0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Minh Nguyễn on 23/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
